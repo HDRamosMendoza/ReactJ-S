b9dd389d225b7be45bd697630c8add735686f5eb
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-08-31 14:50:48 -0500

    Cambio de estado

--- 1_Proyecto/reactpixabay/src/App.js
+++ 1_Proyecto/reactpixabay/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-function Helloworld(props){
+/*function Helloworld(props){
   console.log(props);
   return(
     <div id="hello">
@@ -9,20 +9,60 @@ function Helloworld(props){
       {props.myText}
     </div>
   );
-}
+}*/
 
-/*Estamos reescribiendo la función*/
+/*Estamos reescribiendo la función de varias formas(3 maneras). Componentes.*/
 /*Ambas funciones son equivalentes depende setu criterio*/
 /*
   const App = () => <div>This is my component: <Helloworld/> </div>
 */
-
 /*
 class App extends React.Component {
   render() {
     return <div> This is my component <Helloworld/> </div>
   }
 }*/
+
+class Helloworld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  /*toggleShow(){
+    this.setState({show: false});
+  }*/
+
+  toggleShow = () =>{
+    this.setState({show: !this.state.show});
+  }
+
+  render() {
+    if(this.state.show) {
+      /* Para acceder al props es a partir de un THIS */
+      /*<button onClick={()=> this.setState({show: false})}>*/
+      /*<button onClick={this.toggleShow.bind(this)}>*/
+      return (
+      <div id="hello">
+        <h3>
+          {this.props.subtitle}
+        </h3>
+        {this.props.myText}
+        <button onClick={this.toggleShow}>
+          Cambiar estado
+        </button>
+      </div>
+      )
+    } else {
+      return <h1>
+        No hay elementos
+        <button onClick={this.toggleShow}>
+          Cambiar estado
+        </button>
+      </h1>
+    }
+  }
+}
 
 function App() {
   return (
