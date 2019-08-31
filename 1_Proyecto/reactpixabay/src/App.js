3d56c1177777e030d101fc193e43543766e0b771
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-08-30 22:18:33 -0500

    Reutilizacion de componentes

--- 1_Proyecto/reactpixabay/src/App.js
+++ 1_Proyecto/reactpixabay/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Helloworld(){
+  return(
+    <div id="hello">Hello world</div>
+  );
+}
+
+/*Estamos reescribiendo la función*/
+/*Ambas funciones son equivalentes depende setu criterio*/
+/*
+  const App = () => <div>This is my component: <Helloworld/> </div>
+*/
+
+/*
+class App extends React.Component {
+  render() {
+    return <div> This is my component <Helloworld/> </div>
+  }
+}*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>This is my component: <Helloworld/> </div>
   );
 }
 
