5c31a11bb7c898dd2129c9460826617a2efb06d4
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-08-31 13:52:30 -0500

    Se agrego propiedades de props

--- 1_Proyecto/reactpixabay/src/App.js
+++ 1_Proyecto/reactpixabay/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 
-function Helloworld(){
+function Helloworld(props){
+  console.log(props);
   return(
-    <div id="hello">Hello world</div>
+    <div id="hello">
+      <h3>{props.subtitle}</h3>
+      {props.myText}
+    </div>
   );
 }
 
@@ -22,7 +26,11 @@ class App extends React.Component {
 
 function App() {
   return (
-    <div>This is my component: <Helloworld/> </div>
+    <div>This is my component: 
+      <Helloworld myText="Hola Mundo 1" subtitle="Loremp ispum 1"/>
+      <Helloworld myText="Hola Mundo 2" subtitle="Loremp ispum 2"/>
+      <Helloworld myText="Hola Mundo 3" subtitle="Loremp ispum 3"/>
+    </div>
   );
 }
 
