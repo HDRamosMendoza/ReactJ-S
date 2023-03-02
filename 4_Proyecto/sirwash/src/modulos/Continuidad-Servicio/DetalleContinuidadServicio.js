2fc10a4d9fb9486655bb0450189c02ad6c10b6c2
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2023-03-01 23:38:46 -0500

    Se creo la interfaz de DETALLE CONTINUIDAD DEL SERVICIO

--- 4_Proyecto/sirwash/src/modulos/Continuidad-Servicio/DetalleContinuidadServicio.js
+++ 4_Proyecto/sirwash/src/modulos/Continuidad-Servicio/DetalleContinuidadServicio.js
@@ -19,6 +19,7 @@ import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
 import {Navegacion_03} from '../../Componente/Navegacion';
 import {AccionVolver} from '../../Componente/Accion';
 import Titulo from '../../Componente/Titulo';
+import Typography from '@mui/material/Typography';
 import '../../style/ContinuidadServicio.css';
 
 const NAV_01 = "Lista de Encuestas";
@@ -26,6 +27,22 @@ const URL_01 = "/home";
 const NAV_02 = "Continuidad del Servicio";
 const URL_02 = "/ListaDeEncuestas/ContinuidadServicio";
 const NAV_03 = "Detalle Continuidad del Servicio";
+const itemData = [
+    {
+      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+      title: 'Foto de Declaración Jurada',
+      author: '(obligatorio)',
+    },
+];
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+}));
+
 
 function DetalleContinuidadServicio() {
     return (<div>
@@ -35,6 +52,208 @@ function DetalleContinuidadServicio() {
             nav_02={NAV_02} 
             url_02={URL_02} 
             nav_03={NAV_03} />
+        <Box xs={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Item>
+                        <Box component="span" m={0} 
+                            display="flex" 
+                            justifyContent="space-between" 
+                            alignItems="left" 
+                            xs={1}>
+                            <Grid item xs={11} display="flex" justifyContent="flex-start">
+                            <Titulo name={NAV_03}/>
+                            </Grid>
+                            <Grid item xs={1} display="flex" justifyContent="flex-center" sx={{ padding:'5px 0 0 20px', textAlign: 'center'}}>
+                                <Stack direction="row" spacing={1}>
+                                    <Chip label="Sin Validadar" color="warning" />
+                                </Stack>
+                            </Grid>
+                        </Box>
+                        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)">
+
+                            <Box gridColumn="span 6">
+                                <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}} justifyContent="flex-start">
+                                    <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                        <label>Recolector</label>
+                                    </Box>
+                                    <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+                                        <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry</span>
+                                    </Box>
+                                </Box>
+
+                                <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                    <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                        <label>Centro Poblado</label>
+                                    </Box>
+                                    <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+                                        <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry</span>
+                                    </Box>
+                                </Box>
+
+                                <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                    <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                        <label>Sistema de Agua Potable (SAP)</label>
+                                    </Box>
+                                    <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+                                        <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry</span>
+                                    </Box>
+                                </Box>
+
+                                <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                    <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                        <label>Registro de Continuidad del Servicio</label>
+                                    </Box>
+                                    <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+
+                                        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                            <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                                <label>Mes</label>
+                                            </Box>
+                                            <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+                                                <span>Lorem</span>
+                                            </Box>
+                                        </Box>
+
+                                        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                            <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                                <label>Frecuencia</label>
+                                            </Box>
+                                            <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+                                                <span>Lorem</span>
+                                            </Box>
+                                        </Box>
+
+                                        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                            <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                                <label>Horas</label>
+                                            </Box>
+                                            <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+                                                <span>Lorem </span>
+                                            </Box>
+                                        </Box>
+                                        
+                                    </Box>
+                                </Box>
+
+                                <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                    <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                        <label>Caudal de Agua (litros)</label>
+                                    </Box>
+                                    <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+                                        <span>Lorem Ipsum is simply dummy text</span>
+                                    </Box>
+                                </Box>
+
+                                <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                    <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                        <label>Tiempo de Caudal</label>
+                                    </Box>
+                                    <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+
+                                        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                            <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                                <label>Tiempo 1 (segundos)</label>
+                                            </Box>
+                                            <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+                                                <span>Lorem Ipsum is simply dummy</span>
+                                            </Box>
+                                        </Box>
+
+                                        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                            <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                                <label>Tiempo 2 (segundos)</label>
+                                            </Box>
+                                            <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+                                                <span>Lorem Ipsum is simply dummy</span>
+                                            </Box>
+                                        </Box>
+
+                                        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                            <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                                <label>Tiempo 3 (segundos)</label>
+                                            </Box>
+                                            <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+                                                <span>Lorem Ipsum is simply dummy</span>
+                                            </Box>
+                                        </Box>
+
+                                    </Box>
+                                </Box>
+
+                                <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                    <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                        <label>Fecha de Registro</label>
+                                    </Box>
+                                    <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+                                        <span>10/02/2023</span>
+                                    </Box>
+                                </Box>
+
+                                <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                    <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                        <label>Sustento</label>
+                                    </Box>
+                                    <Box gridColumn="span 8"  sx={{padding:'0', margin: '0'}}>
+                                        <List sx={{padding:0}}>
+                                            <ListItem sx={{padding:'0 0 0 5px'}}>
+                                                <ListItemIcon>
+                                                    <AddAPhotoIcon sx={{fontSize: 17}}/>
+                                                </ListItemIcon>
+                                                <ListItemText secondary="Foto de Declaración Jurada (obligatorio)"/>
+                                            </ListItem>
+                                        </List>
+
+                                        <ImageList sx={{ width: 1, height: 250, padding:'0', margin:'10px 0 0 0' }}>
+                                            {itemData.map((item) => (
+                                                <ImageListItem key={item.img}>
+                                                <img src={`${item.img}?w=248&fit=crop&auto=format`}
+                                                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                                    alt={item.title}
+                                                    loading="lazy"/>
+                                                <ImageListItemBar
+                                                    title={item.title}
+                                                    subtitle={item.author}
+                                                    actionIcon={
+                                                        <IconButton
+                                                            sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                                                            aria-label={`info about ${item.title}`} title="DESCARGAR">
+                                                            <CloudDownloadIcon />
+                                                        </IconButton>
+                                                    }/>
+                                                </ImageListItem>
+                                            ))}
+                                        </ImageList>
+                                    </Box>
+                                </Box>
+
+                                <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}  sx={{p:1}}>
+                                    <Box gridColumn="span 4" sx={{ textAlign:'right', fontWeight: 'bold' }}>
+                                        <label>Observaciones</label>
+                                    </Box>
+                                    <Box gridColumn="span 8" sx={{ textAlign:'left'}}>
+                                        <span>Lorem Ipsum is simply dummy text of the printingy</span>
+                                    </Box>
+                                </Box>
+
+                            </Box>
+
+                            <Box gridColumn="span 6" sx={{ p:2, border:'1px dashed grey' }}>
+                                MAPA
+                            </Box>
+
+                        </Box>
+                        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2} sx={{p:2}} >
+                            <Box gridColumn="span 12">                  
+                            </Box>
+                        </Box>
+                        <Divider variant="middle" sx={{ width: '99%' }}/>
+                        <AccionVolver _volver={URL_02} />
+                    </Item>
+                </Grid>
+            </Grid>
+        </Box>
+
     </div>)
 };
 
--- 4_Proyecto/sirwash/src/modulos/Medicion-Cloro-Residual/DetalleMedicionCloroResidual.js
+++ 4_Proyecto/sirwash/src/modulos/Medicion-Cloro-Residual/DetalleMedicionCloroResidual.js
@@ -38,6 +38,7 @@ const itemData = [
         author: '(obligatorio)',
     },
 ];
+
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
